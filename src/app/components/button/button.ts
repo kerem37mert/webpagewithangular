de75744eba3d4e15f414a767e1162ab1966d0d91
kerem37mert <kerem37mert@yahoo.com>
2026-08-06 17:05:56 +0300

    Add props to button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -2,6 +2,8 @@ import {Component, input} from "@angular/core";
 import {RouterLink} from '@angular/router';
 
 type btnType = "link" | "button";
+type btnVariant = "primary" | "secondary";
+type btnSize = "small" | "medium" | "large";
 
 @Component({
   selector: "app-button",
@@ -13,7 +15,9 @@ type btnType = "link" | "button";
 })
 
 export class Button {
-  text = input<string | number>();
   type = input.required<btnType>();
+  text = input<string | number>();
   link = input<string>();
+  variant = input<btnVariant>("primary");
+  size = input<btnSize>("medium");
 }
