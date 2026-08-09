cb2e8a02b93aead9628c28f5d27dd8b74abfa71d
kerem37mert <kerem37mert@yahoo.com>
2026-08-09 20:14:36 +0300

    update edit note

--- src/app/components/edit-note/edit-note.ts
+++ src/app/components/edit-note/edit-note.ts
@@ -1,15 +1,21 @@
 import {Component, model} from "@angular/core";
 import {Modal} from '../modal/modal';
+import {Button} from '../button/button';
 
 @Component({
   selector: "app-edit-note",
   templateUrl: "./edit-note.html",
   styleUrls: ["./edit-note.scss"],
   imports: [
-    Modal
+    Modal,
+    Button
   ]
 })
 
 export class EditNote {
   isModalOpen = model<boolean>(false);
+
+  onClose(): void {
+    this.isModalOpen.set(false);
+  }
 }
