5e91599a901bcc7a59ba56dcfdec07dfcd0a9702
kerem37mert <kerem37mert@yahoo.com>
2026-08-16 13:49:26 +0300

    add colors

---
+++ src/constants/colors/index.ts
@@ -0,0 +1,16 @@
+export interface ColorType {
+  color: string;
+  hex?: string;
+  rgb?: string;
+}
+
+export const colors: ColorType[] = [
+  {
+    color: "white",
+    hex: "#fff",
+  },
+  {
+    color: "blue",
+    hex: "#add8e6",
+  }
+];
