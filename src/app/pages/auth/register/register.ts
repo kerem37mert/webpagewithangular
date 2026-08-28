d9968f76c804b7c5ce6f114af27c2252cccb709d
kerem37mert <kerem37mert@yahoo.com>
2026-08-28 11:26:31 +0300

    update

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,11 +1,25 @@
-import { Component } from "@angular/core";
+import {Component, signal} from "@angular/core";
+import {RegisterForm} from '../../../../constants';
+import {form} from '@angular/forms/signals';
+import TextInput from '../../../components/text-input/text-input';
 
 @Component({
   selector: "app-register",
   templateUrl: "./register.html",
   styleUrls: ["./register.scss"],
+  imports: [
+    TextInput
+  ]
 })
 
 export class Register {
+  registerModel = signal<RegisterForm>({
+    email: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+  })
 
+  registerForm = form(this.registerModel);
 }
