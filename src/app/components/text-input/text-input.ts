a9c67f63ac7d8c680b239cba980b4d33236701a5
kerem37mert <kerem37mert@yahoo.com>
2026-08-28 12:54:39 +0300

    update

--- src/app/components/text-input/text-input.ts
+++ src/app/components/text-input/text-input.ts
@@ -1,6 +1,9 @@
 import {Component, input} from "@angular/core";
 import {FieldTree, FormField} from '@angular/forms/signals';
 
+type inputSize = "small" | "medium" | "large";
+type inputType = "text" | "password";
+
 @Component({
   selector: "app-text-input",
   templateUrl: "./text-input.html",
@@ -14,4 +17,6 @@ export default class TextInput {
   placeholder = input<string>("");
   field = input<FieldTree<string>>();
   label = input<string>();
+  size = input<inputSize>("medium");
+  type = input<inputType>("text");
 }
